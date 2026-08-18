329bd7963183f09493b9e4f029ecd82fc05b2d66
Rand0Bot <HarrisR8.RO@gmail.com>
2026-08-18 22:56:18 +0100

    updated game loop to .kill() both the asteroid and shot when you shoot an asteroid

--- main.py
+++ main.py
@@ -39,6 +39,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+                    break
             
         for sprite in drawable:
             sprite.draw(screen)
