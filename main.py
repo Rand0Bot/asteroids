def3260eff509bb1ed6e4229d5cf840805176491
Rand0Bot <HarrisR8.RO@gmail.com>
2026-08-18 22:04:38 +0100

    added asteroids and the ability to shoot. modified other files to allow this.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from logger import log_event, log_state
+from shot import Shot
+import sys
+
 
 
 def main():
@@ -10,9 +16,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
 
     while True:
@@ -22,6 +34,12 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            
         for sprite in drawable:
             sprite.draw(screen)
 
