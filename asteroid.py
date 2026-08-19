c6187738d50ba9b778a95f35857290445896111a
Rand0Bot <HarrisR8.RO@gmail.com>
2026-08-19 09:03:46 +0100

    updated asteroids to split if bigger than the smallest asteroid radius

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,13 @@
-import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import pygame
+import random
+
+
+
+
+
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -11,3 +18,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        first_velocity = self.velocity.rotate(angle)
+        second_velocity = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        child1 = Asteroid(self.position.x, self.position.y, new_radius)
+        child2 = Asteroid(self.position.x, self.position.y, new_radius)
+        child1.velocity = first_velocity * 1.2
+        child2.velocity = second_velocity * 1.2
+
+
+
+
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
                     break
             
         for sprite in drawable:
